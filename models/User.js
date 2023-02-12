be2ae3cd48135715db314c34cdff8072db8cd591
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-02-12 12:21:19 +0200

    User schema has default news, weather, xlnks vals;

--- models/User.js
+++ models/User.js
@@ -18,8 +18,19 @@ const userSchema = new mongoose.Schema({
     admin: {
         type: Boolean,
         default: false,
+    },
+    news: {
+        type: Array,
+        default: ['World', 'United States', 'Politics', 'Business', 'Science', 'Sports', 'Technology', 'Education']
+    },
+    weather: {
+        type: Array,
+        default: ['Seattle', 'Tel Aviv', 'New York', 'Los Angeles', 'Chicago', 'Paris', 'London', 'Paris', 'Berlin', 'Tokyo']
+    },
+    externalLinks: {
+        type: Array,
+        default: ['www.google.com', 'www.facebook.com', 'www.linkedin.com', 'www.youtube.com', 'www.instagram.com', 'www.reddit.com']
     }
-    
 })
 
 // Export the schema
