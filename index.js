746db7385f3589e42317c3f6bc7064ec9fe384af
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-02-13 16:01:53 +0200

    +clg colors;

--- index.js
+++ index.js
@@ -37,3 +37,14 @@ app.put("/updateInfo", authController.updateInfo);
 
 // Run the server on port 8000 with a console.log to tell the backend "developer"
 app.listen(8000, () => console.log("Starting connection to port 8000."));
+
+
+// ++++++++++++++++++ Console.log() commands to change colors of the output: ++++++++++++++++++
+// console.log('\x1b[31m%s\x1b[0m', 'This text is red.');
+// console.log('\x1b[32m%s\x1b[0m', 'This text is green.');
+// console.log('\x1b[33m%s\x1b[0m', 'This text is yellow.');
+// console.log('\x1b[34m%s\x1b[0m', 'This text is blue.');
+// console.log('\x1b[35m%s\x1b[0m', 'This text is magenta.');
+// console.log('\x1b[36m%s\x1b[0m', 'This text is cyan.');
+// console.log('\x1b[37m%s\x1b[0m', 'This text is white.');
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
