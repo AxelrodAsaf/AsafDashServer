c1e346625084b98ea2094c989ea0c418e93ae654
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-02-13 16:01:38 +0200

    *updateInfo - simplifying vars; cleaning;

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -71,7 +71,7 @@ exports.token = async (req, res, next) => {
                 // Verify the JWT
                 payload = jwt.verify(token, secretKey);
                 // Access the payload of the JWT
-                console.log(`Verified token of email: ${payload.email}`);
+                console.log('\x1b[32m%s\x1b[0m', `Verified token of user: ${payload.email}`);
                 // Add the user's email (from the token) to the request
                 req.user = payload.email;
             } catch (error) {
@@ -86,9 +86,16 @@ exports.token = async (req, res, next) => {
 
 // When the client calls '/getInfo' using the 'GET' method, the server responds with a JSON array of all the relevant info from the database
 exports.getInfo = async (req, res) => {
-    // If there isn't a user's email in the request, return an error
+    // If there isn't a user's email in the request, find the 'default' user in the database
     if (req.user === undefined) {
-        res.status(404).json({ message: "No user found" });
+        // Find the 'default' user in the database
+        const user = await User.findOne({ email: 'default' });
+        topic = req.params.topic;
+        topic = topic.toLowerCase();
+        const result = user[topic];
+        // Send the default data to the client
+        console.log(`\x1b[35m%s\x1b[0m`, `'${topic}' default data sent to a user`);
+        res.status(200).json(result);
     }
     // If an email is in the request, try to find the user in the database and retrieve the news array
     else {
@@ -96,9 +103,10 @@ exports.getInfo = async (req, res) => {
         try {
             const user = await User.findOne({ email: req.user });
             topic = req.params.topic;
-            topic = topic.toLowerCase();
-            const result = user[topic];
+            const lowerTopic = topic.toLowerCase();
+            const result = user[lowerTopic];
             // Send the result array to the client
+            console.log(`\x1b[35m%s\x1b[0m`, `'${lowerTopic}' sent to '${user.email}'`);
             res.status(200).json(result);
         }
         // There was an email in the request, but the user was not found in the database, or another error occurred
@@ -113,35 +121,33 @@ exports.getInfo = async (req, res) => {
 exports.updateInfo = async (req, res) => {
     // If there isn't a user's email in the request, return an error
     if (req.user === undefined) {
-        res.status(404).json({ message: "No user found" });
+        return res.status(404).json({ message: "No user found" });
     }
-    // If an email is in the request, try to find the user in the database
-    else {
-        // Using the email given in the request, find the user
-        try {
-            const user = await User.findOne({ email: req.user });
-            // Define the variable named 'topic' from the body of the request
-            var topic = req.headers.topic;
-            topic = topic.toLowerCase();
-            var updateArray = req.headers.updatearray.split(',').map(s => s.trim().toLowerCase());
-            // Update the database with the array 'updateArray' under the key 'topic'
-            user[topic] = updateArray;
-            // Save the user to the database
-            user.save((err) => {
-                if (err) {
-                    return res.status(500).json({ message: err.message });
-                }
-                else {
-                    console.log(user);
-                    return res.status(200).json(user);
-                }
-            });
-        }
-        // There was an email in the request, but the user was not found in the database, or another error occurred
-        catch (error) {
-            console.error(error.message);
-            res.status(500).json({ message: error.message });
-        }
-    }
-}
 
+
+    try {
+        // Find the user in the database using their email
+        const user = await User.findOne({ email: req.user });
+        // Check to see if there is a topic in the request headers
+        if (req.headers.topic === undefined) {
+            return res.status(400).json({ message: "No topic specified" });
+        }
+        // Retrieve the 'topic' variable from the request headers
+        const topic = req.headers.topic.toLowerCase();
+        // Retrieve the 'updateArray' variable from the request body
+        const updateArray = req.body.updateArray.map(s => s.trim());
+        // Update the user's data in the database with the new 'updateArray' array under the key 'topic'
+        user[topic] = updateArray;
+        // Save the user to the database
+        user.save((err) => {
+            if (err) {
+                return res.status(500).json({ message: err.message });
+            }
+            console.log(`\x1b[33m%s\x1b[0m`, `'${topic}' updated for user: ${user.email}`);
+            return res.status(200).json(user);
+        });
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: error.message });
+    }
+};
